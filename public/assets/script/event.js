68ab4e81afb58178ec98c62af4632c578707fcf5
Zack Shone <zackariahshone@gmail.com>
2020-09-11 20:22:38 -0700

    commit before pull from test

---
+++ public/assets/script/event.js
@@ -0,0 +1,12 @@
+const APIKEY = "58f7590b78mshb2a60492e836525p15bc37jsna6355455c4b7";
+
+
+$.ajax({
+    type: "GET",
+    url: "https://www.eventbriteapi.com/v3/categories/?token=GWFXNZZUTNCKCDN34HUJ",
+    data: "data",
+}).then(functions(response){
+
+})
+
+    
