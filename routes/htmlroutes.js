e87022425c72c820c92b8d6d22fc9ec22eda17fb
Blake Arambula <bfire23@gmail.com>
2020-09-15 20:42:15 -0700

    passport

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,5 +1,7 @@
 const path = require("path");
 
+const isAuthenticated = require("../config/middleware/isAuthenticated");
+
 
 // Routes
 // =============================================================
@@ -24,5 +26,10 @@ module.exports = function(app) {
   app.get("/stacheup", function(reg, res){
       res.sendFile(path.join(__dirname, "../public/html/signup.html"));
   })
+// Here we've add our isAuthenticated middleware to this route.
+  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/signup", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
 
 };
