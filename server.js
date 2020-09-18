84d7eefbc67c5fe1ed030437585cf67636a1da2d
Blake Arambula <bfire23@gmail.com>
2020-09-17 20:25:45 -0700

    updated server.js

--- server.js
+++ server.js
@@ -24,11 +24,10 @@ app.use(routes);
 require("./routes/htmlroutes.js")(app);
 app.use(express.static('public'));
 
-// Creating express app and configuring middleware needed for authentication
-// const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static("public"));
+
+
+
+
 // We need to use sessions to keep track of our user's login status
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 app.use(passport.initialize());
